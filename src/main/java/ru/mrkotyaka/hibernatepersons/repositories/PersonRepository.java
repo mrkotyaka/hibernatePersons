05593e51722a2c8e91cbabe0c05cd1dc7110cc83
mrkotyaka <mrkotyaka@gmail.com>
2025-07-25 22:46:12 +0300

    refactor with JPA @Query

--- src/main/java/ru/mrkotyaka/hibernatepersons/repositories/PersonRepository.java
+++ src/main/java/ru/mrkotyaka/hibernatepersons/repositories/PersonRepository.java
@@ -1,6 +1,8 @@
 package ru.mrkotyaka.hibernatepersons.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.mrkotyaka.hibernatepersons.entities.Person;
 
@@ -10,9 +12,12 @@ import java.util.Optional;
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Integer> {
 
-    List<Person> findByCityOfLiving(String city);
+    @Query("select p from Person p where p.cityOfLiving = :city")
+    List<Person> findByCityOfLiving(@Param("city") String city);
 
-    List<Person> findByAgeLessThanOrderByAge(int age);
+    @Query("select p from Person p where p.age < :age order by p.age")
+    List<Person> findByAgeLessThanOrderByAge(@Param("age") int age);
 
-    Optional<List<Person>> findByNameAndSurname(String name, String surname);
+    @Query("select p from Person p where p.name = :name and p.surname = :surname")
+    Optional<List<Person>> findByNameAndSurname(@Param("name") String name, @Param("surname") String surname);
 }
